2ada563f115d5f012aa374fac63d5d2717cdc108
Hesham Saif <heshamsaif856@gmail.com>
2025-09-23 04:27:17 +0300

    add title

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,17 @@
+// pages/_app.js
 import "../styles/globals.css";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
+      <Head>
+        <title>السلطان</title>
+        {/* <meta name="description" content="أفضل موقع لتجربة Next.js" /> */}
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/logo.png" />
+      </Head>
+
       <Component {...pageProps} />
     </>
   );
